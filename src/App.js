3bbe6ab0beee5735758618f2fe9fdbf89eae461e
quandhagithub <quandhade140119@fpt.edu.vn>
2023-06-01 20:19:06 +0700

    create project init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './layouts/sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/sidebar/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./sidebar.module.css";
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <a href="#" className={styles.active}>
+        Home
+      </a>
+      <a href="#">Services</a>
+      <a href="#">News</a>
+      <a href="#">Blog</a>
+      <a href="#">Contact</a>
+    </div>
+  );
+};
+
+export default Sidebar;
